dba5a751bae7d04555c59ef3dcaa223221533904
vovan <vovan@BOBA>
2023-05-31 14:45:28 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace izuchenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("ХУЙ");
+            //Console.WriteLine("пенис");
+            Thread.Sleep(1000);
+            int a;
+            a = 5;
+            Console.WriteLine(a);
+           
+        }
+    }
+}
